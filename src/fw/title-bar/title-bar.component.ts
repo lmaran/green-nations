1f7b95baf326a421bf78b4212522f003fb4be60a
Lucian Maran <lucian.maran@outlook.com>
2017-06-17 18:02:37 +0300

    first commit

---
+++ src/fw/title-bar/title-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+// we don't need ScreenService if 'ScreenLarge' directive is used
+// import { ScreenService } from '../services/screen.service';
+import { MenuService } from '../services/menu.service';
+
+@Component({
+  selector: 'fw-title-bar',
+  templateUrl: './title-bar.component.html',
+  styleUrls: ['./title-bar.component.css']
+})
+export class TitleBarComponent implements OnInit {
+
+  constructor(
+    //private screenService: ScreenService
+    private menuService: MenuService) { }
+
+  ngOnInit() {
+  }
+
+}
